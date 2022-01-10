9115a57be69671be3ba3f9c1bd608535307a7326
ahmid <ahmidsanni@gmail.com>
2022-01-10 02:30:35 -0800

    first commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const IndexPage = () => (
-  <h1 className="text-3xl font-bold underline text-red-100">Hello world!</h1>
+  <h1 className="text-3xl font-bold underline text-red-100">Welcome to hotel dashboard!</h1>
 )
 
 export default IndexPage
