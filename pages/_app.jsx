9e7792ef50df19bb9593bd2ec0a7c797b20e590c
CyberDevv <tocyberdevv@gmail.com>
2022-01-10 16:19:16 +0100

    added progessbar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,10 @@
+import NextNProgress from 'nextjs-progressbar'
+
 import '../styles/global.css'
 
 const App = ({ Component, pageProps }) => (
   <>
+    <NextNProgress height={5} color="#03B575" />
     <Component {...pageProps} />
   </>
 )
