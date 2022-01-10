2f3c17d57bb60aa8d067c982ca55e57cd6315d5b
CyberDevv <tocyberdevv@gmail.com>
2022-01-10 13:40:30 +0100

    Made some changes

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const IndexPage = () => (
+  <h1 className="text-3xl font-bold underline text-red-700">Welcome to hotel dashboard!</h1>
+)
+
+export default IndexPage
--- src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const IndexPage = () => (
-  <h1 className="text-3xl font-bold underline text-red-100">Welcome to hotel dashboard!</h1>
-)
-
-export default IndexPage
